b8c4e3326298b8f054058f7c5d0b19e1cbac920a
JegadishTamizh <74832347+JegadishTamizh@users.noreply.github.com>
2021-10-20 02:00:12 +0530

    Update ffmpeg.py

--- helpers/ffmpeg.py
+++ helpers/ffmpeg.py
@@ -10,9 +10,9 @@ async def extract_audio(client, message, data):
     await message.edit_text("Extracting Stream from file...")
 
     dwld_loc = data['location']
-    out_loc = data['location'] + ".opus"
+    out_loc = data['location'] + ".mka"
 
-    if data['name'] == "opus":
+    if data['name'] == "mka":
         out, err, rcode, pid = await execute(f"ffmpeg -i '{dwld_loc}' -map 0:{data['map']} -c copy '{out_loc}' -y")
         if rcode != 0:
             await message.edit_text("**Error Occured. See Logs for more info.**")
@@ -20,7 +20,7 @@ async def extract_audio(client, message, data):
             await clean_up(dwld_loc, out_loc)
             return
     else:
-        out, err, rcode, pid = await execute(f"ffmpeg -i '{dwld_loc}' -map 0:{data['map']} '{out_loc}' -y")
+        out, err, rcode, pid = await execute(f"ffmpeg -i '{dwld_loc}' -map 0:{data['map']} -c copy '{out_loc}' -y")
         if rcode != 0:
             await message.edit_text("**Error Occured. See Logs for more info.**")
             print(err)
