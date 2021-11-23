cb3fae8b50991d89879f6c659fdeaa4dc657cdd5
JegadishTamizh <74832347+JegadishTamizh@users.noreply.github.com>
2021-11-23 16:06:55 +0530

    Update ffmpeg.py

--- helpers/ffmpeg.py
+++ helpers/ffmpeg.py
@@ -13,14 +13,14 @@ async def extract_audio(client, message, data):
     out_loc = data['location'] + ".mkv"
 
     if data['name'] == "mkv":
-        out, err, rcode, pid = await execute(f"ffmpeg -i '{dwld_loc}' -c copy -an -metadata title='Tamil Fusion' -metadata:s:v:0 title='Tamil Fusion'  -metadata:s:s:0 title='Tamil Fusion' '{out_loc}' -y")
+        out, err, rcode, pid = await execute(f"ffmpeg -i '{dwld_loc}' -map 0 -map -0:{data['map']} -c copy -metadata title='Tamil Fusion' -metadata:s:v title='Tamil Fusion' -metadata:s:a title='Tamil Fusion' -metadata:s:s title='Tamil Fusion' '{out_loc}' -y")
         if rcode != 0:
             await message.edit_text("**Error Occured. See Logs for more info.**")
             print(err)
             await clean_up(dwld_loc, out_loc)
             return
     else:
-        out, err, rcode, pid = await execute(f"ffmpeg -i '{dwld_loc}' -c copy -an -metadata title='Tamil Fusion' -metadata:s:v:0 title='Tamil Fusion' -metadata:s:s:0 title='Tamil Fusion' '{out_loc}' -y")
+        out, err, rcode, pid = await execute(f"ffmpeg -i '{dwld_loc}' -map 0 -map -0:{data['map']} -c copy -metadata title='Tamil Fusion' -metadata:s:v title='Tamil Fusion' -metadata:s:a title='Tamil Fusion' -metadata:s:s title='Tamil Fusion' '{out_loc}' -y")
         if rcode != 0:
             await message.edit_text("**Error Occured. See Logs for more info.**")
             print(err)
@@ -38,7 +38,7 @@ async def extract_subtitle(client, message, data):
     dwld_loc = data['location']
     out_loc = data['location'] + ".mkv"   
 
-    out, err, rcode, pid = await execute(f"ffmpeg -i '{dwld_loc}' -c copy -an -metadata title='Tamil Fusion' -metadata:s:v:0 title='Tamil Fusion'  -metadata:s:s:0 title='Tamil Fusion' '{out_loc}' -y")
+    out, err, rcode, pid = await execute(f"ffmpeg -i '{dwld_loc}' -c copy -an -metadata title='Tamil Fusion' -metadata:s:v title='Tamil Fusion'  -metadata:s:s title='Tamil Fusion' '{out_loc}' -y")
     if rcode != 0:
         await message.edit_text("**Error Occured. See Logs for more info.**")
         print(err)
